864c42b2e1dbc140cfbd59a1e2cad578473b38f1
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 16:35:37 -0500

    Create routes for the user API resource

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Iniciar sesión solo para el dueño.
+router.get('/log-in', function(req,res,next){
+  return res.send('Logged');
+})
+
+// Salir de la sesión (solo aplica para el dueño).
+router.get('/log-out', function(req, res, next){
+  return res.send('Logged out');
+})
+
 module.exports = router;
