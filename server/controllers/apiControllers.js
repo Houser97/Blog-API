194a0219348b20f6633297af65aa24c2edde57fa
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 17:48:43 -0500

    Create api controller to fetch posts in database

--- server/controllers/apiControllers.js
+++ server/controllers/apiControllers.js
@@ -3,5 +3,8 @@ const Comment = require('../models/comment');
 
 // Controlador para buscar posts en base de datos
 exports.fetch_posts = function(req, res, next){
-    
-}
+    Post.find().sort([['title', 'ascending']]).exec(function(err, posts){
+        if(err) return next(err);
+        return res.json({'posts':'Houser'})
+    });
+};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiController = require('../controllers/apiControllers');
+
 // API para buscar todos los POSTS en la base de datos con PUBLISHED TRUE
-router.get('/posts', function(req, res, next){
-    return res.json('API to fetch posts in database');
-});
+router.get('/posts', apiController.fetch_posts);
 
 // API para buscar un determinado POST en la base da datos junto con sus comentarios.
 router.get('/post/:name', function(req, res, next){
@@ -20,3 +20,5 @@ router.post('/post/:name/create-comment', function(req, res, next){
 router.post('/post/:name/create-post', function(req, res, next){
     return res.json('Create new post');
 })
+
+module.exports = router;
