491ed71e3cdb5ef7a1c50ac25b93fa0fac4a0b9d
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 17:13:49 -0500

    Create API routes

---
+++ server/routes/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// API para buscar todos los POSTS en la base de datos con PUBLISHED TRUE
+router.get('/posts', function(req, res, next){
+    return res.send('API to fetch posts in database');
+});
+
+// API para buscar un determinado POST en la base da datos junto con sus comentarios.
+router.get('/post/:name', function(req, res, next){
+    return res.send('API to fetch a single POST and its comments');
+});
+
+// API para crear un nuevo comentario
+router.post('/post/:name/create-comment', function(req, res, next){
+    return res.send('Create new comment');
+})
+
+// API para crear un nuevo POST
+router.post('/post/:name/create-post', function(req, res, next){
+    return res.send('Create new post');
+})
