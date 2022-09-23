18691bd62dd638b77834c20e3e016bc45f4c996d
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 17:26:03 -0500

    Replace res.send with res.json in API routes

--- server/controllers/apiControllers.js
+++ server/controllers/apiControllers.js
@@ -1,2 +1,7 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+
+// Controlador para buscar posts en base de datos
+exports.fetch_posts = function(req, res, next){
+    
+}
