27a1d5085014e1847eba896a1860d371d40e330a
Houser97 <arturo.riverar97@gmail.com>
2022-09-22 21:51:29 -0500

    Set up dotenv and mongoose

--- server/app.js
+++ server/app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Importar DOTENV.
+require('dotenv').config();
+
+// Colocar link de acceso a base de datos.
+const MongoDB = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.hfuvxe2.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+
+// Obtener conexión por defecto.
+const db = mongoose.connection;
+
+// Ligar conexión con evento de error.
+db.on('error', console.error.bind((console, 'MongoDB connection error: ')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
