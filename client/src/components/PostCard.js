783fcf7ec8a3260c66a4b07e29abf8d04f3c137e
Houser97 <arturo.riverar97@gmail.com>
2022-09-24 12:38:15 -0500

    Remove date dependencies in React

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/postCard.css'
+import { DateTime } from 'luxon';
 
 const PostCard = ({title, timestamp}) => {
-  return (
+
+    const [formattedDate, setFormattedDate] = useState(timestamp);
+
+    useEffect(() => {
+        setFormattedDate(prev => DateTime.fromJSDate(prev).toLocaleString(DateTime.DATE_MED))
+    }, [])
+    
+    return (
     <div className='postCard'>
         <div className='title-post-card item-card'>{title}</div>
-        <div className='timestamp-post-card item-card'>{timestamp}</div>
+        <div className='timestamp-post-card item-card'>{formattedDate}</div>
     </div>
-  )
+    )
 }
 
 export default PostCard
