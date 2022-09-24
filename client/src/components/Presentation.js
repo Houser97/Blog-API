d501275875c2c4b87ef1c98fb765d239d770417a
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 21:57:08 -0500

    Set up states in Presentation to write sentences dynamically

--- client/src/components/Presentation.js
+++ client/src/components/Presentation.js
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/presentation.css'
 
 const Presentation = () => {
-  return (
+
+    const sentences = ['Welcome to my Blog!', 'Enjoy your stay!'];
+    const [delta, setDelta] = useState(200);
+    const [isWriting, setIsWriting] = useState(true);
+    const [text, setText] = useState('');
+    const [loopNum, setLoopNum] = useState(0);
+
+    useEffect(() => {
+        let tick = setInterval(() => {
+            writeOrDelete();
+        }, delta)
+
+        return () => {clearInterval(tick)}
+    }, [text])
+
+    const writeOrDelete = () => {
+        let index = loopNum % sentences.length;
+        let fullText = sentences[index];
+        let currentText = isWriting ? fullText.substring(0, text.length+1) : fullText.substring(0, text.length-1)
+
+        setText(currentText);
+
+        if(!isWriting){
+            setDelta(prev => prev/2);
+        }
+        if(!isWriting && currentText === ''){
+            setIsWriting(true);
+            setLoopNum(prev => prev + 1);
+            setDelta(200);
+        }
+        if(isWriting && currentText === fullText){
+            setIsWriting(false);
+            setDelta(2000);
+        }
+    }
+
+    return (
     <section className='presentation'>
-        <h1>
-            Welcome to my Blog!
+        <h1 className='presentation-h1'>
+            {text}
+            <span className='span-h1'></span>
         </h1>
         <p>
             This blog was created as a project for The Odin Project. It involves knowledge about NodeJS as the backend and React as the Frontend.
         </p>
     </section>
-  )
+    )
 }
 
 export default Presentation
