66f76fc14eb4aa419bda3f72792573c012394d1f
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 19:49:54 -0500

    Style Navbar and import it to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='App'>
+      <Navbar />
       App
     </div>
   )
