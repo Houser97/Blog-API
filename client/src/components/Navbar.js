3b2c5e3405dd14fbc274895678cf94d96efd56cf
Houser97 <arturo.riverar97@gmail.com>
2022-09-23 20:13:30 -0500

    Add state to Navbar to add dynamic classes to links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/navbar.css'
 
 const Navbar = () => {
-  return (
+
+    const [activeLink, setActiveLink] = useState('')
+
+    return (
     <nav className='navbar'>
         <div className='navbar-item logo'>My Blog-API</div>
         <div className='navbar-item a-links'>
-            <a className='link-navbar'>Home</a>
-            <a className='link-navbar'>Posts</a>
+            <a className={`link-navbar first-navbar-link 
+            ${activeLink === 'Home' ? 'activeLink' : ''}`} 
+            onClick={() => setActiveLink('Home')}>Home</a>
+
+            <a className={`link-navbar ${activeLink === 'Posts' ? 'activeLink' : ''}`} 
+            onClick={() => setActiveLink('Posts')}>Posts</a>
         </div>
     </nav>
-  )
+    )
 }
 
 export default Navbar
