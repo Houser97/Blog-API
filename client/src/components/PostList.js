4db5c7ef5a85a83b0aaea7d1e2fa86054797812f
Houser97 <arturo.riverar97@gmail.com>
2022-09-24 12:18:25 -0500

    Fetch posts using useEffect and the API created

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/postList.css'
+import PostCard from './PostCard';
 
 const PostList = () => {
+
+  const[backendPosts, setBackendPosts] = useState('');
+
+  useEffect(() => {
+    fetch('/api/posts').
+        then(response => response.json()).
+        then(data => setBackendPosts(data)).
+        catch(() => setBackendPosts('There was an error.'))
+  }, [])
+
   return (
     <section className='posts'>
-        Here goes post cards
+        {backendPosts === '' || backendPosts === 'There was an error' ? 
+        'There was an error' : 
+        backendPosts.map((post, index) => {
+          return(
+            <PostCard key={`post-key-${index}`} {...post}/>
+          )
+        })}
     </section>
   )
 }
