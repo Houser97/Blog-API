88f2edb22087b87c07df4499124c7c738f11f32e
Houser97 <arturo.riverar97@gmail.com>
2022-09-24 22:37:58 -0500

    Format post titles to use them in URL when using Link Router

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/postCard.css'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 const PostCard = ({title, timestamp}) => {
 
-    const [formattedDate, setFormattedDate] = useState(timestamp);
+    const [formattedDate, setFormattedDate] = useState('');
+    const [formattedTitle, setFormattedTitle] = useState('');
 
     useEffect(() => {
         setFormattedDate(moment(timestamp).format('MMM Do yyyy'))
+        setFormattedTitle(title.replace(/\s/g, '-'))
     }, [])
     
     return (
-    <div className='postCard'>
-        <div className='title-post-card item-card'>{title}</div>
-        <div className='timestamp-post-card item-card'>{formattedDate}</div>
-    </div>
+    <Link to={`post/${formattedTitle}`} className = 'Link-Router'>
+        <div className='postCard'>
+            <div className='title-post-card item-card'>{title}</div>
+            <div className='timestamp-post-card item-card'>{formattedDate}</div>
+        </div>
+    </Link>
     )
 }
 
