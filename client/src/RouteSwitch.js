d414fb7e24c787d9e1ec2f9ab642c29a7419c3ab
Houser97 <arturo.riverar97@gmail.com>
2022-09-24 22:24:56 -0500

    Create Post.js to host single posts and import it to RouteSwitch.js

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Post from './Routes/Post'
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element = {<App />} />
-            <Route path='post/:title' />
+            <Route path='post/:title' element = {<Post />}/>
         </Routes>
     </BrowserRouter>
   )
---
+++ client/src/Routes/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const Post = () => {
+  return (
+    <div className='Post-website'>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
+
+export default Post
