b213739b85f0a61c959671cb557896f1868dd94f
Houser97 <arturo.riverar97@gmail.com>
2022-09-25 19:21:08 -0500

    Format timestamp in PostComponent using Moment

--- client/src/components/PostComponent.js
+++ client/src/components/PostComponent.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/PostComponent.css'
+import moment from 'moment'
 
 const PostComponent = ({title, body, timestamp}) => {
+
+  const [formattedTime, setFormattedTime] = useState(timestamp)
+
+  useEffect(() => {
+    setFormattedTime(moment(timestamp).format('MMMM Do yyyy'))
+  }, [])
+  
+
   return (
     <div className='post-component'>
         <h2 className='post-component-title'>{title}</h2>
         <div className='post-component-body'>{body}</div>
-        <div className='post-component-timestamp'>{timestamp}</div>
+        <div className='post-component-timestamp'>{formattedTime}</div>
     </div>
   )
 }
