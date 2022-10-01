390ccb2fce20962e32d8d55882f0ede9051135f0
Houser97 <arturo.riverar97@gmail.com>
2022-09-30 21:16:16 -0500

    Create comments section component and its stylesheet

---
+++ client/src/components/commentsSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/commentsSection.css'
+
+const commentsSection = () => {
+  return (
+    <div className='comments-section'>commentsSection</div>
+  )
+}
+
+export default commentsSection
