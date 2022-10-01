23089465349470ff381c0c998a1d1e78dc18cccf
Houser97 <arturo.riverar97@gmail.com>
2022-10-01 13:46:43 -0500

    Import Navbar and Footer components into Login

--- client/src/Routes/Login.js
+++ client/src/Routes/Login.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import '../styles/Login.css'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 const Login = () => {
   return (
     <div className='login-section'>
-        <form className='form-login'>
-            <div className='username-section form-login-div'>
-                <label htmlFor='username' className='username-label label-login'>Username:</label>
-                <input className='input-username input-login' name='username' id='username'></input>
-            </div>
-            <div className='pwd-section form-login-div'>
-                <label htmlFor='pwd' className='username-label label-login'>Username:</label>
-                <input type='password' className='input-username input-login' name='password' id='pwd'></input>
-            </div>
-            <button className='login-button'>Submit</button>
-        </form>
+      <Navbar />
+      <form className='form-login'>
+          <div className='username-section form-login-div'>
+              <label htmlFor='username' className='username-label label-login'>Username:</label>
+              <input className='input-username input-login' name='username' id='username'></input>
+          </div>
+          <div className='pwd-section form-login-div'>
+              <label htmlFor='pwd' className='username-label label-login'>Username:</label>
+              <input type='password' className='input-username input-login' name='password' id='pwd'></input>
+          </div>
+          <button className='login-button'>Submit</button>
+      </form>
+      <Footer />
     </div>
   )
 }
