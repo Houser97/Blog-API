feffc209cf5859c231cad8ed9b084204e38547a3
Houser97 <arturo.riverar97@gmail.com>
2022-10-01 13:44:16 -0500

    Create Login stylesheet and import it to RouteSwitch

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 import App from './App'
+import Login from './components/Login'
 import Post from './Routes/Post'
 
 const RouteSwitch = () => {
@@ -9,6 +10,7 @@ const RouteSwitch = () => {
         <Routes>
             <Route path='/' element = {<App />} />
             <Route path='post/:title' element = {<Post />}/>
+            <Route path='/login' element = {<Login />} />
         </Routes>
     </BrowserRouter>
   )
