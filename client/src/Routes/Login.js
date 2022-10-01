2a9f8a63cdee179eee60aefc2d8316bb59b503da
Houser97 <arturo.riverar97@gmail.com>
2022-10-01 14:00:05 -0500

    Style login form

--- client/src/Routes/Login.js
+++ client/src/Routes/Login.js
@@ -7,17 +7,22 @@ const Login = () => {
   return (
     <div className='login-section'>
       <Navbar />
-      <form className='form-login'>
-          <div className='username-section form-login-div'>
-              <label htmlFor='username' className='username-label label-login'>Username:</label>
-              <input className='input-username input-login' name='username' id='username'></input>
+      <div className='form-login-wrapper'>
+        <form className='form-login'>
+          <div className='login-title'>
+            Login
           </div>
-          <div className='pwd-section form-login-div'>
-              <label htmlFor='pwd' className='username-label label-login'>Username:</label>
-              <input type='password' className='input-username input-login' name='password' id='pwd'></input>
-          </div>
-          <button className='login-button'>Submit</button>
-      </form>
+            <div className='username-section form-login-div'>
+                <label htmlFor='username' className='username-label label-login'>Username:</label>
+                <input className='input-username input-login' name='username' id='username'></input>
+            </div>
+            <div className='pwd-section form-login-div'>
+                <label htmlFor='pwd' className='username-label label-login'>Password:</label>
+                <input type='password' className='input-username input-login' name='password' id='pwd'></input>
+            </div>
+            <button className='login-button'>Submit</button>
+        </form>
+      </div>
       <Footer />
     </div>
   )
