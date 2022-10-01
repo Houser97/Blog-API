5ab4b8a8b78d3d18aab815ae45954ba4c30e62e5
Houser97 <arturo.riverar97@gmail.com>
2022-09-30 22:28:33 -0500

    Add hidden input in form to pass in post title

--- client/src/components/CommentsSection.js
+++ client/src/components/CommentsSection.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/commentsSection.css'
 
-const CommentsSection = () => {
+const CommentsSection = ({title}) => {
   return (
     <div className='comments-section'>
         <form className='create-comment-form'>
             <textarea className='textarea' placeholder='Write a comment'></textarea>
+            <input type='hidden' value={title} name = 'post-title'></input>
             <button className='submit-comment'>Submit</button>
         </form>
 
