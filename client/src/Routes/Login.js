f227cbaac9c7bec99b515240cef36e25dc90dd86
Houser97 <arturo.riverar97@gmail.com>
2022-10-01 14:32:43 -0500

    Declare function to send data to API for login

--- client/src/Routes/Login.js
+++ client/src/Routes/Login.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 const Login = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const LoginApi = (e) => {
+    e.preventDefault()
+    if(!username || !password){return false}
+    fetch('/api/login', {
+      method: 'Post',
+      header: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+    })
+  }
+  
   return (
     <div className='login-section'>
       <Navbar />
       <div className='form-login-wrapper'>
-        <form className='form-login'>
+        <form method='POST' className='form-login' onSubmit={(e) => LoginApi(e)}>
           <div className='login-title'>
             Are you the owner? Then login to edit your posts!
           </div>
             <div className='username-section form-login-div'>
                 <label htmlFor='username' className='username-label label-login'>Username:</label>
-                <input className='input-username input-login' name='username' id='username'></input>
+                <input className='input-username input-login' name='username' id='username'
+                onChange={(e) => setUsername(e.target.value)}></input>
             </div>
             <div className='pwd-section form-login-div'>
                 <label htmlFor='pwd' className='username-label label-login'>Password:</label>
-                <input type='password' className='input-username input-login' name='password' id='pwd'></input>
+                <input type='password' className='input-username input-login' name='password' id='pwd'
+                onCanPlay={(e) => setPassword(e.target.value)}></input>
             </div>
             <button className='login-button'>Submit</button>
         </form>
