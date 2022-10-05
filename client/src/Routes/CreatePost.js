3663604ad89d16bf3c5d31fabe560c9ee29852bb
Houser97 <arturo.riverar97@gmail.com>
2022-10-04 20:29:57 -0500

    Import Editor from TinyMCE to CreatePost

--- client/src/Routes/CreatePost.js
+++ client/src/Routes/CreatePost.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/CreatePost.css'
+import {Editor} from '@tinymce/tinymce-react'
 
 const CreatePost = () => {
   return (
@@ -13,6 +14,10 @@ const CreatePost = () => {
               <label htmlFor='title-post-form' className='label-form-create'>Title</label>
               <input id='title-post-form' className='input-form-post' name='title-post'></input>
             </div>
+            <Editor 
+              textareaName='post-body'
+              initialValue='Write post comment'
+            />
           </form>
         </div>
         <Footer />
