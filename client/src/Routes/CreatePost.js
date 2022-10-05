e4ab4fc5464b89b2e2dc72449bc33e3fc4080dea
Houser97 <arturo.riverar97@gmail.com>
2022-10-04 20:32:48 -0500

    Add init parameter to Editor component in CreatePost

--- client/src/Routes/CreatePost.js
+++ client/src/Routes/CreatePost.js
@@ -21,6 +21,21 @@ const CreatePost = () => {
             <Editor 
               textareaName='post-body'
               initialValue='Write post comment'
+              onEditorChange={newText => setBody(newText)}
+              init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                  'advlist autolink lists link image charmap print preview anchor',
+                  'searchreplace visualblocks code fullscreen',
+                  'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar: 'undo redo | formatselect | ' +
+                'bold italic backcolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+              }}
             />
           </form>
         </div>
