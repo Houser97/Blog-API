256d2e96955a4b92184384b1233f26e0c6f86632
Houser97 <arturo.riverar97@gmail.com>
2022-10-04 20:26:28 -0500

    Style form for the creation of posts

--- client/src/Routes/CreatePost.js
+++ client/src/Routes/CreatePost.js
@@ -7,12 +7,14 @@ const CreatePost = () => {
   return (
     <section className='CreatePost-section'>
         <Navbar isInHome={false} />
-        <form method='POST' className='create-form-post'>
-          <div className='div-form-create form-post-title'>
-            <label htmlFor='title-post-form' className='label-form-create'>Title</label>
-            <input id='title-post-form' className='input-form-post' name='title-post'></input>
-          </div>
-        </form>
+        <div className='form-post-wrapper'>
+          <form method='POST' className='create-form-post'>
+            <div className='div-form-create form-post-title'>
+              <label htmlFor='title-post-form' className='label-form-create'>Title</label>
+              <input id='title-post-form' className='input-form-post' name='title-post'></input>
+            </div>
+          </form>
+        </div>
         <Footer />
     </section>
   )
