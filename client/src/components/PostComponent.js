6d6e4d58aaae32f5658e8bd1c2d85b05401838a6
Houser97 <arturo.riverar97@gmail.com>
2022-10-08 15:55:01 -0500

    Install html-react-parser dependency

--- client/src/components/PostComponent.js
+++ client/src/components/PostComponent.js
@@ -6,17 +6,22 @@ import CommentsSection from './CommentsSection'
 const PostComponent = ({title, body, timestamp}) => {
 
   const [formattedTime, setFormattedTime] = useState(timestamp)
+  const [bodyParsed, setBodyParsed] = useState(new DOMParser().parseFromString(body, 'text/xml'))
 
   useEffect(() => {
     setFormattedTime(moment(timestamp).format('MMMM Do yyyy'))
   }, [])
+  
+  useEffect(() => {
+    console.log(bodyParsed)
+  }, [bodyParsed])
   
 
   return (
     <div className='post-component'>
       <div className='post-component-article'>
         <h2 className='post-component-title'>{title}</h2>
-        <div className='post-component-body'>{body}</div>
+        <div className='post-component-body'>{bodyParsed.firstChild.innerHTML}</div>
         <div className='post-component-timestamp'>{formattedTime}</div>
       </div>
 
