4f828b80cd14c3ad9c6059572805a34314c3f08c
Houser97 <arturo.riverar97@gmail.com>
2022-10-09 12:43:35 -0500

    Use HTMLReactParser to read html tags from db to PostComponent

--- client/src/components/PostComponent.js
+++ client/src/components/PostComponent.js
@@ -2,26 +2,22 @@ import React, { useEffect, useState } from 'react'
 import '../styles/PostComponent.css'
 import moment from 'moment'
 import CommentsSection from './CommentsSection'
+import HTMLReactParser from 'html-react-parser'
 
 const PostComponent = ({title, body, timestamp}) => {
 
   const [formattedTime, setFormattedTime] = useState(timestamp)
-  const [bodyParsed, setBodyParsed] = useState(new DOMParser().parseFromString(body, 'text/xml'))
+  //const [bodyParsed, setBodyParsed] = useState(new DOMParser().parseFromString(body, 'text/xml'))
 
   useEffect(() => {
     setFormattedTime(moment(timestamp).format('MMMM Do yyyy'))
   }, [])
-  
-  useEffect(() => {
-    console.log(bodyParsed)
-  }, [bodyParsed])
-  
 
   return (
     <div className='post-component'>
       <div className='post-component-article'>
         <h2 className='post-component-title'>{title}</h2>
-        <div className='post-component-body'>{bodyParsed.firstChild.innerHTML}</div>
+        <div className='post-component-body'>{HTMLReactParser(body)}</div>
         <div className='post-component-timestamp'>{formattedTime}</div>
       </div>
 
