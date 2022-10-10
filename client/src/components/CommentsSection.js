9bc9bc5600ba352e36a0438c76ccd79fb28305aa
Houser97 <arturo.riverar97@gmail.com>
2022-10-09 20:13:14 -0500

    Create comment state to store textarea data on change

--- client/src/components/CommentsSection.js
+++ client/src/components/CommentsSection.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/commentsSection.css'
 
 const CommentsSection = ({idPost}) => {
+
+  const [comment, setComment] = useState('')
+
   useEffect(() =>{
     console.log(idPost)
   })
+
   return (
     <div className='comments-section'>
         <form className='create-comment-form'>
-            <textarea className='textarea' placeholder='Write a comment'></textarea>
+            <textarea className='textarea' placeholder='Write a comment'
+            onChange={(e) => setComment(e.target.value)}></textarea>
             <input type='hidden' value={idPost} name = 'idPost'></input>
             <button className='submit-comment'>Submit</button>
         </form>
