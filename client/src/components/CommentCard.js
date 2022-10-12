efcdec7bf262b3bc714a73abe729b6dc55ed29a1
Houser97 <arturo.riverar97@gmail.com>
2022-10-11 21:25:23 -0500

    Set grid in CommentCard to define 2 columns

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -9,7 +9,7 @@ const CommentCard = ({data}) => {
     }) 
   return (
     <div className='comment-card'>
-        <div className='username-comment-dov'>{username}</div>
+        <div className='username-comment-div'>{username}</div>
         <div className='time-comment-divs'>
             <div className='timestamp-comment-div comment-div'>{comment}</div>
             <div className='comment-comment-div comment-div'>{timestamp}</div>
