23023f3638426517293bc570fabc77d3e3baa9c9
Houser97 <arturo.riverar97@gmail.com>
2022-10-14 20:35:09 -0500

    Set up fetch operation to get post to edit

--- client/src/Routes/Edit.js
+++ client/src/Routes/Edit.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 const Edit = () => {
+
+    const [post, setPost] = useState(null)
+    const {title} = useParams()
+
+    useEffect(() => {
+        fetch(`/api/post/${title}`)
+        .then(response => response.json())
+        .then(data => setPost(() => data))
+        .catch(() => setPost(null))
+    }, [])
+
+
   return (
     <div className='Edit-page'>
         <Navbar isInHome={false} isInLogIn = {false} />
