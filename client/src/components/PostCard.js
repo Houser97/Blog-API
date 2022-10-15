db2d625bf64c555978eacc7dd32b1db78fc89f6b
Houser97 <arturo.riverar97@gmail.com>
2022-10-15 14:00:19 -0500

    Create function to handle classes of prevention msg

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -23,11 +23,15 @@ const PostCard = ({title, timestamp, _id}) => {
         .then(data => console.log(data))
     }
     
+    const handleDeleteMsg = () => {
+        setshowMsg((prev) => !prev)
+    }
+
     return (
     <div className='postCard'>
         {isToken ? (
             <div className='container-edit-delete'>
-                <svg className='svg-edit-delete' viewBox="0 0 24 24">
+                <svg className='svg-edit-delete' viewBox="0 0 24 24" onClick={() => handleDeleteMsg()}>
                     <path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
                 </svg>
                 <a href={`post/edit/${formattedTitle}`} className = 'a-svg-edit-delete'>
@@ -45,7 +49,7 @@ const PostCard = ({title, timestamp, _id}) => {
                 Are you sure you want to delete this post?
                 <div className='buttons-are-u-sure'>
                     <button className='btn-form-sure yes-btn'>Yes</button>
-                    <button className='btn-form-sure no-btn'>No</button>
+                    <div className='btn-form-sure no-btn' onClick={() => handleDeleteMsg()}>No</div>
                 </div>
             </form>
         </div>
