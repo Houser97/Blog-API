56aa7e2fcb2c05b0f246dc5cf4e63a03bad81283
Houser97 <arturo.riverar97@gmail.com>
2022-10-16 16:47:21 -0500

    Add unpublished link to redirect to posts that are not visible

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,7 +19,12 @@ const Navbar = ({isInHome, isInLogIn}) => {
                 <a href = '#posts' className={`link-navbar ${activeLink === 'Posts' ? 'activeLink' : ''}`} 
                 onClick={() => setActiveLink('Posts')}>Posts</a>
 
-                {isToken ? ( <a href='/create-post' className='create-link'>Create</a>):(
+                {isToken ? ( 
+                    <div className='token-home'>
+                        <a href = '/unpublished-posts' className='unpublished-link'>Unpublished</a>
+                        <a href='/create-post' className='create-link'>Create</a>
+                    </div>
+                ):(
                     <a href='/login' className='sign-in'>Log in</a>
                 )}
             </div>
@@ -32,6 +37,7 @@ const Navbar = ({isInHome, isInLogIn}) => {
                 isToken ? ( 
                     <div className='navbar-item a-links'>
                         <a href = '/' className={`link-navbar first-navbar-link`}>Home</a>
+                        <a href = '/unpublished-posts' className={`link-navbar first-navbar-link`}>Unpublished</a>
                         <a href='/create-post' className='create-link'>Create</a>
                     </div>
                     ):(
