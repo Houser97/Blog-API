81225f9572ea4f2b026c5f5a86927556f30c7159
Houser97 <arturo.riverar97@gmail.com>
2022-10-20 20:38:47 -0500

    Create navbar for small devices

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -70,6 +70,47 @@ const Navbar = ({isInHome, isInLogIn}) => {
                 <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
             </svg>
         </div>
+        
+        <div className='navbar-sm'>
+            {isInHome ? (
+                <div className='navbar-item-sm a-links-sm'>
+                    <a href = '#home' className={`link-navbar first-navbar-link 
+                    ${activeLink === 'Home' ? 'activeLink' : ''}`} 
+                    onClick={() => setActiveLink('Home')}>Home</a>
+
+                    <a href = '#posts' className={`link-navbar ${activeLink === 'Posts' ? 'activeLink' : ''}`} 
+                    onClick={() => setActiveLink('Posts')}>Posts</a>
+
+                    {isToken ? ( 
+                        <div className='token-home'>
+                            <a href = '/unpublished-posts' className='unpublished-link'>Unpublished</a>
+                            <a href='/create-post' className='create-link'>Create</a>
+                        </div>
+                    ):(
+                        <a href='/login' className='sign-in'>Log in</a>
+                    )}
+                </div>
+            ) : (
+                isInLogIn ? (
+                    <div className='navbar-item a-links'>
+                        <a href = '/' className={`link-navbar first-navbar-link`}>Home</a>
+                    </div>
+                ):(
+                    isToken ? ( 
+                        <div className='navbar-item a-links'>
+                            <a href = '/' className='link-navbar first-navbar-link'>Home</a>
+                            <a href = '/unpublished-posts' className='link-navbar first-navbar-link'>Unpublished</a>
+                            <a href='/create-post' className='create-link'>Create</a>
+                        </div>
+                        ):(
+                        <div className='navbar-item a-links'>
+                            <a href = '/' className={`link-navbar first-navbar-link`}>Home</a>
+                            <a href='/login' className='sign-in'>Log in</a>
+                        </div>
+                    )
+                )
+            )}
+        </div>
     </nav>
     )
 }
