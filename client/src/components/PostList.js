52f301c2b3b8243d79c449e71c3676b52f7f1760
Houser97 <arturo.riverar97@gmail.com>
2022-10-23 16:27:30 -0500

    Add class to post section according to process of fetching data

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -5,16 +5,20 @@ import PostCard from './PostCard';
 const PostList = () => {
 
   const[backendPosts, setBackendPosts] = useState('');
+  const [postClass, setPostClass] = useState(false)
 /*
   useEffect(() => {
     fetch('/api/posts').
         then(response => response.json()).
-        then(data => setBackendPosts(data)).
+        then(data => {
+          setBackendPosts(data)
+          setPostClass(true)
+        }).
         catch(() => setBackendPosts('There was an error.'))
   }, [])
 */
   return (
-    <section id='posts' className='posts'>
+    <section id='posts' className={`${postClass ? 'posts':'no-posts'}`}>
         {backendPosts === '' || backendPosts === 'There was an error' ? 
         <div className='error-posts'>
           Loading...
