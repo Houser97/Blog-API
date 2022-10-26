5e7af0c6cb6325b0cb4df5141e5d7ad9f7713b2f
Houser97 <arturo.riverar97@gmail.com>
2022-10-25 21:33:19 -0500

    Remove unused import for deleted stylesheet

--- client/src/Routes/UnpublishedPosts.js
+++ client/src/Routes/UnpublishedPosts.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import UnpublishedList from '../components/UnpublishedList'
-import '../styles/UnpublishedPosts.css'
 
 const UnpublishedPosts = () => {
   return (
