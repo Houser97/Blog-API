b70182882f153103f1b57761e147be7fda2688d5
Houser97 <arturo.riverar97@gmail.com>
2022-10-25 20:48:38 -0500

    Disable React DevTools in production

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import RouteSwitch from './RouteSwitch';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
