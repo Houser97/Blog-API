42dcbc1331ecea5513a9a37a338b6b763a20a7d6
Houser97 <arturo.riverar97@gmail.com>
2022-10-29 11:17:57 -0500

    Place the catch all code after the app use of the /api route

--- server/app.js
+++ server/app.js
@@ -38,11 +38,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', apiRouter);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
-
-app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
