83e903971a830e27fd01d941a7065aaaa08575e8
Houser97 <arturo.riverar97@gmail.com>
2022-10-28 23:37:28 -0500

    Remove root path for the API fetching

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -16,7 +16,7 @@ const RouteSwitch = () => {
 
   useEffect(() => {
     if(token){
-      fetch('https://blog-api-k3qd.onrender.com/api/check-token', {
+      fetch('/api/check-token', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
