31bdbea265cfed2d1a9590d1697ff859885689d1
Houser97 <arturo.riverar97@gmail.com>
2022-10-28 23:35:39 -0500

    Remove unused code

--- server/app.js
+++ server/app.js
@@ -16,9 +16,6 @@ if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
 };
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const postRouter = require('./routes/post');
 const apiRouter = require('./routes/api');
 
 // Colocar link de acceso a base de datos.
@@ -32,8 +29,8 @@ const db = mongoose.connection;
 db.on('error', console.error.bind((console, 'MongoDB connection error: ')))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -45,10 +42,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/post', postRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
